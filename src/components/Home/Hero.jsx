4d9e57a8919e16b7e113a499bf71fde9f1637230
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-10-16 21:53:05 +0400

    finished hero section

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (
         <div className="container container__hero" id="hero" style={{height:"500px", backgroundColor:"black"}}>
-       <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, quas?</p></div>
+       <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
         </div>
     )
 }
