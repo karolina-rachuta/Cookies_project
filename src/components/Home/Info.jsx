e42737934349770f8de71d4acbbe48d9f16cb56f
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-10-17 21:51:03 +0400

    info section finished with styles and layout

--- src/components/Home/Info.jsx
+++ src/components/Home/Info.jsx
@@ -1,7 +1,10 @@
 const Info = () => {
     return (
         <div className="container container__info" id="info">
-       <div><p>Lorem ipsum dolor sit amet.</p></div>
+            <div className="info__box">
+            <div className="info__lead1"><p>Lorem ipsum dolor</p></div>
+            <div className="info__lead2"><p>dolor sit amet sit</p></div>
+            </div>
         </div>
     )
 }
