89182db3052175e0f76b4d3e2078e2234b6612a0
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-10-17 21:06:07 +0400

    info section

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
     return (
-        <div className="container container__hero" id="hero" style={{height:"500px", backgroundColor:"black"}}>
+        <div className="container container__hero" id="hero">
        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></div>
         </div>
     )
--- src/components/Home/Info.jsx
+++ src/components/Home/Info.jsx
@@ -1,7 +1,7 @@
 const Info = () => {
     return (
-        <div className="container" id="info" style={{height:"500px", backgroundColor:"pink"}}>
-        <h1>INFO</h1>
+        <div className="container container__info" id="info">
+       <div><p>Lorem ipsum dolor sit amet.</p></div>
         </div>
     )
 }
